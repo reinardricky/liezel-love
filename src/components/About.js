b36423d2499f0474a2195ddea53f2b5d77a4ba9e
reinardricky <reinardricky@gmail.com>
2024-07-05 18:00:27 +0700

    Add images for journey

--- src/components/About.js
+++ src/components/About.js
@@ -3,14 +3,18 @@ import React from 'react';
 const About = () => (
   <div className="p-12">
     <h1 className="text-4xl text-pink-500 text-center mb-8">Our Journey</h1>
-    <div className="text-center mb-8"><img src="photo1.jpg" alt="Photo of us" className="w-1/2 mx-auto rounded-lg" /></div>
+    <div className="text-center mb-8"><img src="/images/bee.svg" alt="bumble" className="w-1/2 mx-auto rounded-lg" /></div>
     <div className="mb-8">
       <p className="text-lg text-center">Remember when we first met? [Short anecdote]</p>
     </div>
-    <div className="text-center mb-8"><img src="photo2.jpg" alt="Another photo" className="w-1/2 mx-auto rounded-lg" /></div>
+    <div className="text-center mb-8"><img src="/images/cinema.svg" alt="cinema" className="w-1/2 mx-auto rounded-lg" /></div>
     <div className="mb-8">
       <p className="text-lg text-center">And this was one of our best moments... [Short anecdote]</p>
     </div>
+    <div className="text-center mb-8"><img src="/images/sing.svg" alt="karaoke" className="w-1/2 mx-auto rounded-lg" /></div>
+    <div className="text-center mb-8"><img src="/images/cards.svg" alt="cards" className="w-1/2 mx-auto rounded-lg" /></div>
+    <div className="text-center mb-8"><img src="/images/hug.svg" alt="hug" className="w-1/2 mx-auto rounded-lg" /></div>
+    <div className="text-center mb-8"><img src="/images/kiss.svg" alt="kiss" className="w-1/2 mx-auto rounded-lg" /></div>
   </div>
 );
 
